0fb5560fbc45bd656f1810f6a301bc2512ee856e
Jacob Turner <me@jacobturner.me>
2017-04-21 14:05:21 -0500

    code from today's demo

--- src/edu/hendrix/csci250/csci250proj4/gui/PictoChatController.java
+++ src/edu/hendrix/csci250/csci250proj4/gui/PictoChatController.java
@@ -15,10 +15,10 @@ import javafx.concurrent.Task;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -29,17 +29,16 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.StrokeLineCap;
-import javafx.stage.Stage;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 
 public class PictoChatController {
@@ -48,12 +47,21 @@ public class PictoChatController {
 	@FXML
 	private MenuItem quitMenuItem;
 	@FXML
+	private ScrollPane chatroomScrollPane;
+	@FXML
 	private VBox chatroomContents;
 	@FXML
 	private Pane drawingCanvas;
+	@FXML
+	private Button sendButton;
+	@FXML
+	private ColorPicker chatColor;
 	
 	private ServerSocket serverSocket;
 	private Socket server;
+	private User user;
+	private double sx;
+	private double sy;
 
 	@FXML
 	private void initialize() {
@@ -63,12 +71,43 @@ public class PictoChatController {
 		} catch (IOException e) {
 			e.printStackTrace();
 			outputMessage(AlertType.ERROR, e.getMessage());
+			
+		}
+		chatColor.setValue(Color.BLACK);
+		drawingCanvas.setOnMousePressed(event -> startDrag(event));
+		drawingCanvas.setOnMouseDragged(event -> draw(event));
+		drawingCanvas.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+		Dialog<User> dialog = new Dialog<>();
+		dialog.setTitle("Settings");
+		dialog.setHeaderText("PictoChat Settings");
+		ButtonType buttonTypeSave = new ButtonType("Save", ButtonData.OK_DONE);
+		dialog.getDialogPane().getButtonTypes().addAll(buttonTypeSave, ButtonType.CANCEL);
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		TextField name = new TextField();
+		DatePicker birthdayPicker = new DatePicker();
+		ColorPicker favoriteColor = new ColorPicker();
+		grid.add(new Label("Name:"), 0, 0);
+		grid.add(name, 1, 0);
+		grid.add(new Label("Birthday:"), 0, 1);
+		grid.add(birthdayPicker, 1, 1);
+		grid.add(new Label("Color:"), 0, 2);
+		grid.add(favoriteColor, 1, 2);
+		dialog.getDialogPane().setContent(grid);
+		dialog.setResultConverter(dialogButton -> {
+		    if (dialogButton == buttonTypeSave) {
+		        return new User(name.getText(), birthdayPicker.getValue(), favoriteColor.getValue());
+		    }
+		    return null;
+		});
+		Optional<User> userResult = dialog.showAndWait();
+		if (userResult.isPresent()){
+			user = userResult.get();
+		} else {
 			Platform.exit();
 			System.exit(0);
 		}
-		drawingCanvas.setOnMousePressed(event -> draw(event));
-		drawingCanvas.setOnMouseDragged(event -> draw(event));
-		drawingCanvas.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
 		Alert alert = new Alert(AlertType.CONFIRMATION);
 		alert.setTitle("Welcome to PictoChat");
 		alert.setHeaderText("Welcome to PictoChat");
@@ -77,57 +116,26 @@ public class PictoChatController {
 		ButtonType buttonTypeB = new ButtonType("B");
 		ButtonType buttonTypeC = new ButtonType("C");
 		ButtonType buttonTypeD = new ButtonType("D");
-		ButtonType buttonTypeSettings = new ButtonType("Settings");
 		ButtonType buttonTypeCancel = new ButtonType("Quit", ButtonData.CANCEL_CLOSE);
-		alert.getButtonTypes().setAll(buttonTypeA, buttonTypeB, buttonTypeC, buttonTypeD, buttonTypeSettings, buttonTypeCancel);
+		alert.getButtonTypes().setAll(buttonTypeA, buttonTypeB, buttonTypeC, buttonTypeD, buttonTypeCancel);
 		Optional<ButtonType> result = alert.showAndWait();
 		if (result.get() == buttonTypeA){
 		} else if (result.get() == buttonTypeB) {
 		} else if (result.get() == buttonTypeC) {
 		} else if (result.get() == buttonTypeD) {
-		} else if (result.get() == buttonTypeSettings) {
-			Dialog<User> dialog = new Dialog<>();
-			dialog.setTitle("Settings");
-			dialog.setHeaderText("PictoChat Settings");
-			ButtonType buttonTypeSave = new ButtonType("Save", ButtonData.OK_DONE);
-			dialog.getDialogPane().getButtonTypes().addAll(buttonTypeSave, ButtonType.CANCEL);
-			GridPane grid = new GridPane();
-			grid.setHgap(10);
-			grid.setVgap(10);
-			TextField name = new TextField();
-			DatePicker birthdayPicker = new DatePicker();
-			ColorPicker favoriteColor = new ColorPicker();
-			grid.add(new Label("Name:"), 0, 0);
-			grid.add(name, 1, 0);
-			grid.add(new Label("Birthday:"), 0, 1);
-			grid.add(birthdayPicker, 1, 1);
-			grid.add(new Label("Color:"), 0, 2);
-			grid.add(favoriteColor, 1, 2);
-			dialog.getDialogPane().setContent(grid);
-			dialog.setResultConverter(dialogButton -> {
-			    if (dialogButton == buttonTypeSave) {
-			        return new User(name.getText(), birthdayPicker.getValue(), favoriteColor.getValue());
-			    }
-			    return null;
-			});
-			Optional<User> userResult = dialog.showAndWait();
-			if (userResult.isPresent()){
-			} else {
-			}
 		} else {
 			Platform.exit();
 			System.exit(0);
 		}
-		afterInitialize();
+		new Thread(receiveMessage).start();
 	}
 	
-	private void afterInitialize() {
-		((Node)drawingCanvas.getScene()).setOnKeyPressed(event -> {if(event.getCode().equals(KeyCode.ENTER)){sendMessage();}});
+	public void startDrag(MouseEvent event) {
+		sx = event.getX();
+		sy = event.getY();
 	}
 	
 	public void draw(MouseEvent event) {
-		double sx = event.getX();
-		double sy = event.getY();
 		double fx = event.getX();
 		double fy = event.getY();
 		double canvasWidth = drawingCanvas.getWidth();
@@ -143,7 +151,7 @@ public class PictoChatController {
 			fy = canvasHeight;
 		}
 		Line line = new Line(sx, sy, fx, fy);
-		line.setStroke(Color.BLACK);
+		line.setStroke(chatColor.getValue());
 		line.setStrokeLineCap(StrokeLineCap.ROUND);
 		line.setStrokeWidth(5);
 		drawingCanvas.getChildren().add(line);
@@ -152,10 +160,13 @@ public class PictoChatController {
 	}
 	
 	public void sendMessage() {
+		WritableImage savedPane = drawingCanvas.snapshot(new SnapshotParameters(), null);
+		chatroomContents.getChildren().add(new ImageView(savedPane));
+		drawingCanvas.getChildren().clear();
+		chatroomScrollPane.setVvalue(chatroomScrollPane.getVmax());
 		Socket client;
 		try {
 			client = new Socket("localhost", 8888);
-			WritableImage savedPane = drawingCanvas.snapshot(new SnapshotParameters(), null);
 			ImageIO.write(SwingFXUtils.fromFXImage(savedPane, null), "jpg", client.getOutputStream());
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -166,10 +177,11 @@ public class PictoChatController {
 	Task<Void> receiveMessage = new Task<Void>() {
         @Override
         protected Void call() throws Exception {
-            server = serverSocket.accept();
-			BufferedImage img = ImageIO.read(ImageIO.createImageInputStream(server.getInputStream()));
-			chatroomContents.getChildren().add(new ImageView(SwingFXUtils.toFXImage(img, null)));
-            return null;
+        	while(true) {
+	            server = serverSocket.accept();
+				BufferedImage img = ImageIO.read(ImageIO.createImageInputStream(server.getInputStream()));
+				chatroomContents.getChildren().add(new ImageView(SwingFXUtils.toFXImage(img, null)));
+        	}
         }
     };
 	
